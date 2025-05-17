241d254b3ff1944459ae4e47047d891aff3fdc5c
AliRezaHM <126397239+alirezahairi@users.noreply.github.com>
2025-05-17 14:35:04 +0330

    Refactoring. create UserAuthenticationService

--- src/IdentityProvider.Basic/Program.cs
+++ src/IdentityProvider.Basic/Program.cs
@@ -2,31 +2,55 @@ using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddDataProtection(options => { options.ApplicationDiscriminator = "IdentityProvider.Basic"; });
+builder.Services.AddTransient<UserAuthenticationService>();
 
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context, IDataProtectionProvider dataProtectionProvider) =>
+app.MapGet("/", (UserAuthenticationService userAuthenticationService) =>
 {
-    if (!context.Request.Cookies.TryGetValue("username", out var username))
-        return Results.NotFound();
-
-    var protector = dataProtectionProvider.CreateProtector("IdentityProvider.Basic_Protect-Username");
-    var protectedUsername = protector.Unprotect(username);
-
-    return Results.Ok(protectedUsername);
+    var username = userAuthenticationService.GetUsername();
+    return username != null ? Results.Ok(username) : Results.NotFound();
 });
 
-app.MapPost("/login/{username}",
-    (string username, HttpContext context, IDataProtectionProvider dataProtectionProvider) =>
-    {
-        var protector = dataProtectionProvider.CreateProtector("IdentityProvider.Basic_Protect-Username");
-        var protectedUsername = protector.Protect(username);
-
-        context.Response.Cookies.Append("username", protectedUsername, new CookieOptions()
-        {
-            Expires = DateTime.UtcNow.AddHours(5)
-        });
-    });
+app.MapPost("/login/{username}", (
+    string username,
+    UserAuthenticationService userAuthenticationService) =>
+{
+    userAuthenticationService.SignIn(username);
+});
 
 app.Run();
+
+internal class UserAuthenticationService(
+    IHttpContextAccessor httpContextAccessor,
+    IDataProtectionProvider dataProtectionProvider)
+{
+    private const string CookieUserName = "username";
+    private const string PurposeProtector = "IdentityProvider.Basic_Protect-Username";
+    private readonly HttpContext _httpContext = httpContextAccessor.HttpContext!;
+
+    public void SignIn(string username)
+    {
+        var protector = dataProtectionProvider.CreateProtector(PurposeProtector);
+        var protectedUsername = protector.Protect(username);
+
+        _httpContext.Response.Cookies.Append(CookieUserName, protectedUsername,
+            options: new CookieOptions()
+            {
+                Expires = DateTime.UtcNow.AddHours(5)
+            });
+    }
+
+    public string? GetUsername()
+    {
+        if (!_httpContext.Request.Cookies.TryGetValue(CookieUserName, out var username))
+            return null;
+
+        var protector = dataProtectionProvider.CreateProtector("IdentityProvider.Basic_Protect-Username");
+        var protectedUsername = protector.Unprotect(username);
+
+        return protectedUsername;
+    }
+}
