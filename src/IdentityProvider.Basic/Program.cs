26eb9d22c16f633daaca4565e56f924f42c9d94f
AliRezaHM <126397239+alirezahairi@users.noreply.github.com>
2025-05-17 14:16:52 +0330

    Login with cookie

--- src/IdentityProvider.Basic/Program.cs
+++ src/IdentityProvider.Basic/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection(options => { options.ApplicationDiscriminator = "IdentityProvider.Basic"; });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (HttpContext context, IDataProtectionProvider dataProtectionProvider) =>
+{
+    if (!context.Request.Cookies.TryGetValue("username", out var username))
+        return Results.NotFound();
+
+    var protector = dataProtectionProvider.CreateProtector("IdentityProvider.Basic_Protect-Username");
+    var protectedUsername = protector.Unprotect(username);
+
+    return Results.Ok(protectedUsername);
+});
+
+app.MapPost("/login/{username}",
+    (string username, HttpContext context, IDataProtectionProvider dataProtectionProvider) =>
+    {
+        var protector = dataProtectionProvider.CreateProtector("IdentityProvider.Basic_Protect-Username");
+        var protectedUsername = protector.Protect(username);
+
+        context.Response.Cookies.Append("username", protectedUsername, new CookieOptions()
+        {
+            Expires = DateTime.UtcNow.AddHours(5)
+        });
+    });
 
 app.Run();
