bc582f83db6d8676d9437382094d52e6db042c7e
AliRezaHM <126397239+alirezahairi@users.noreply.github.com>
2025-05-18 09:27:59 +0330

    Authorize with policy

--- src/IdentityProvider.AuthenticationWithAuthorization/Program.cs
+++ src/IdentityProvider.AuthenticationWithAuthorization/Program.cs
@@ -1,4 +1,46 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("idp")
+    .AddCookie("idp", options =>
+    {
+        options.LoginPath = "/login/a";
+        options.Cookie.Name = "idp";
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("idp", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim(ClaimTypes.Name);
+    });
+});
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", (HttpContext httpContext) =>
+{
+    var username = httpContext.User.Identity?.Name;
+
+    return Results.Ok($"username: {username}");
+}).RequireAuthorization("idp");
+
+app.MapPost("/login/{username}", async (
+    string username, HttpContext httpContext) =>
+{
+    var claims = new List<Claim> { new(ClaimTypes.Name, username) };
+    var claimsIdentity = new ClaimsIdentity(claims, "idp");
+    var userPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+    await httpContext.SignInAsync("idp",userPrincipal);
+
+    return Results.Ok("success login");
+});
 
 app.Run();
