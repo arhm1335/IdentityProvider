2f0d4327a0a763d7aba3fe42fae8e7035a398e8a
AliRezaHM <126397239+alirezahairi@users.noreply.github.com>
2025-05-18 17:00:24 +0330

    login and register with microsoft Identity

---
+++ src/IdentityProvider.Identity/Endpoints/Endpoints.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityProvider.Identity.Endpoints;
+
+public static class Endpoints
+{
+    public static void ConfigureEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var identity = endpoints.MapGroup("api/account/");
+
+        identity.MapPost("register/{username}",
+            async (UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, string username) =>
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = username,
+                    Email = "email@email.email",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0915654578",
+                    PhoneNumberConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, "ali@Password123");
+
+                var roleExist = await roleManager.RoleExistsAsync("admin");
+                if (!roleExist)
+                    await roleManager.CreateAsync(new IdentityRole("admin"));
+                
+                await userManager.AddToRoleAsync(user, "admin");
+            });
+
+        identity.MapGet("login/{username}", async (SignInManager<IdentityUser> signInManager,string username) =>
+        {
+            var result = await signInManager.PasswordSignInAsync(username, "ali@Password123", true, false);
+            if(result.Succeeded)
+                return Results.Ok("User Signin Success");
+
+            return Results.BadRequest();
+        });
+    }
+}
--- src/IdentityProvider.Identity/Program.cs
+++ src/IdentityProvider.Identity/Program.cs
@@ -1,4 +1,38 @@
+using IdentityProvider.Identity.Endpoints;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<IdentityDbContext>(configure =>
+    configure.UseInMemoryDatabase("idp.identity"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // User settings
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Sign-in settings
+    options.SignIn.RequireConfirmedEmail = true;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+}).AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+app.ConfigureEndpoints();
 
 app.Run();
